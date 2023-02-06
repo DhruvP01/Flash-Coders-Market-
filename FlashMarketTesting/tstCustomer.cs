587cd1f7981f0e0a780410c33d94779c3b9a7574
Aisha2112 <P2615579@my365.dmu.ac.uk>
2023-02-06 17:11:08 +0000

    set the project up and created database

---
+++ FlashMarketTesting/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using FlashMarketClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FlashMarketTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // create an instance of the class we are making
+            clsCustomer ACustomer = new clsCustomer();
+            // test to see it actually exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
